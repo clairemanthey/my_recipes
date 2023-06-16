36622b852ab1797d6b13d35e997224f9837fb63a
clairemanthey <clairemanthey@gmail.com>
2023-06-16 15:37:44 -0500

    added screenshot

---
+++ src/components/foot.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Footer = () => {
+	return(
+      <footer className="footer">
+        <p>Built with <a href="https://www.gatsbyjs.com">Gatsby</a></p>
+      </footer>
+		)
+}
+
+export default Footer
